1836e784de8cab4fe2d77353e427a725891dff1e
Julian V <jvesper95@gmail.com>
2022-05-30 14:18:43 +0200

    Add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// First file containing traces to unify
+    trace_file_1: std::path::PathBuf,
+    /// Second file containing traces to unify
+    trace_file_2: std::path::PathBuf,
+    /// File providing all valid basic blocks which are used to unify trace files
+    valid_bb_file: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    // TODO: Read and parse all files
+
+    // TODO: Auto-detect trace format ((mmio?), bb, (ram?))
+
+    // TODO: Rewrite by removing all invalid basic blocks
+
+    Ok(())
 }
